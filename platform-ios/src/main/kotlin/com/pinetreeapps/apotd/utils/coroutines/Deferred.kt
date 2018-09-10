a1d60b46ff96c496e708da56d62e360a2ba85531
Michał Szyszka <michalszyszka.m@gmail.com>
2018-09-10 12:49:50 +0200

    Added iOS specific coroutines code.

---
+++ platform-ios/src/main/kotlin/com/pinetreeapps/apotd/utils/coroutines/Deferred.kt
@@ -0,0 +1,16 @@
+package com.pinetreeapps.apotd.utils.coroutines
+
+import kotlin.coroutines.experimental.*
+import kotlin.coroutines.experimental.intrinsics.*
+
+actual class Deferred<out T>(private val context: CoroutineContext,
+                             private val block: suspend () -> T) {
+
+    actual suspend fun await(): T {
+        return suspendCoroutineOrReturn { continuation ->
+            block.startCoroutine(WrappedContinuation(context, continuation))
+            COROUTINE_SUSPENDED
+        }
+    }
+
+}
